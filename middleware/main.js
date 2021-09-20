de5716184651aa62e57ca844714cc25782683075
Heydar Gerayzade <heydar.g@bestcomp.net>
2021-09-20 10:54:11 +0400

    redirect to ref page for auth users as well

--- middleware/main.js
+++ middleware/main.js
@@ -1,6 +1,6 @@
 export default function ({ app, route, redirect }) {
   if(!app.getRouteBaseName() && !route.fullPath.match(/^(\/ru\/)|^(\/ru$)/)) {
-    redirect(app.$resolveRoute('/ru' + route.fullPath, app.i18n.locale || 'az'));
+    return redirect(app.$resolveRoute('/ru' + route.fullPath, app.i18n.locale || 'az'));
   }
   return true;
 }
