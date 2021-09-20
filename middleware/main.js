a52af5f664782d4f9ad75889eb0eb4b16791ca4e
Heydar Gerayzade <heydar.g@bestcomp.net>
2021-09-20 13:15:41 +0400

    fix

--- middleware/main.js
+++ middleware/main.js
@@ -1,6 +1,7 @@
 export default function ({ app, route, redirect }) {
   if(!app.getRouteBaseName() && !route.fullPath.match(/^(\/ru\/)|^(\/ru$)/)) {
-    return redirect(app.$resolveRoute('/ru' + route.fullPath, app.i18n.locale || 'az'));
+    let path = app.$resolveRoute('/ru' + route.fullPath, app.i18n.locale || 'az');
+    if (path !== '/') return redirect(path);
   }
   return true;
 }
