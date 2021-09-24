e32e5bf14d4995a3e0c877f0646264625c7348ab
Heydar Gerayzade <heydar.g@bestcomp.net>
2021-09-24 12:16:42 +0400

    images slider fix

--- components/elements/ImagesSlider.vue
+++ components/elements/ImagesSlider.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="images-slider">
     <div class="toolbar">
-      <span></span>
+      <span>{{ currentSlide + 1 }} / {{ slides.main.length }}</span>
       <span class="cursor-pointer" @click.stop="$emit('close')">
         <icon name="cross" />
       </span>
     </div>
-    <div :class="['container', { 'wider': hasSidebar}]">
+    <div class="container wider">
       <div :class="['row', { 'has-sidebar': hasSidebar}]" @click.stop>
         <div class="col-auto">
           <button id="slider-prev" class="btn" @click.stop="thumbsPrev" v-if="slides.main.length > 6">
@@ -29,7 +29,7 @@
         </div>
         <div class="col-auto">
           <div class="position-relative">
-            <div id="images-swiper" class="swiper-container" v-swiper:imagesSwiper="swiperOps" ref="imagesSwiper">
+            <div id="images-swiper" class="swiper-container" v-swiper:imagesSwiper="swiperOps" ref="imagesSwiper" :auto-update="false">
               <div class="swiper-wrapper">
                 <div class="swiper-slide" :key="index" v-for="(slide, index) in slides.main">
                   <div class="swiper-slide-bg">
@@ -59,7 +59,7 @@
           </div>
         </div>
         <div class="col-auto" v-if="hasSidebar">
-          <slot />
+          <slot name="sidebar" />
         </div>
       </div>
     </div>
@@ -76,7 +76,6 @@ export default {
   },
   data() {
     return {
-      slideIndex: this.currentSlide,
       swiperOps: {
         initialSlide: this.currentSlide,
         effect: 'fade',
@@ -136,6 +135,7 @@ export default {
         this.showIframe = false;
         this.$nextTick(() => {
           this.showIframe = true;
+          this.$emit('slide-change', this.imagesSwiper.realIndex);
         });
       });
     });
