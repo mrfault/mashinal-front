c51514a323ec24790303118cecc217880a5ba718
gesha97 <heydar.g@bestcomp.net>
2021-10-21 17:39:23 +0400

    fix

--- components/parts/Banners.vue
+++ components/parts/Banners.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="row">
     <div class="col-lg-6 mb-2 mb-lg-0">
-      <nuxt-link class="d-block" :to="$localePath('/business-profile?type=1')">
+      <nuxt-link class="d-block" :to="$localePath('/business-profile?type=' + (reverse ? 2 : 1))">
         <img class="round-4 full-width" :src="`/img/create-${reverse ? 'shop' : 'salon'}-${type}-${locale}.jpg`" alt="" />
       </nuxt-link>
     </div>
     <div class="col-lg-6">
-      <nuxt-link class="d-block" :to="$localePath('/business-profile?type=2')">
+      <nuxt-link class="d-block" :to="$localePath('/business-profile?type=' + (reverse ? 1 : 2))">
         <img class="round-4 full-width" :src="`/img/create-${reverse ? 'salon' : 'shop'}-${type}-${locale}.jpg`" alt="" />
       </nuxt-link>
     </div>
