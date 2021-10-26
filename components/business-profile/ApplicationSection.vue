48b138f1d8ff30a863bc5fbe6a808455d02161fc
gesha97 <heydar.g@bestcomp.net>
2021-10-26 15:12:40 +0400

    fixes

--- components/business-profile/ApplicationSection.vue
+++ components/business-profile/ApplicationSection.vue
@@ -9,10 +9,10 @@
         <div class="col-12 col-lg-4 middle-images">
           <div class="row">
             <div class="col-auto col-lg-12">
-              <img :src="`/img/business-profile/application_form_1_${colorMode}.svg`" alt="">
+              <img :src="`/img/business-profile/application_form_1_${colorMode}_${locale}.png`" alt="">
             </div>
             <div class="col-auto col-lg-12">
-              <img :src="`/img/business-profile/application_form_2_${colorMode}.png`" alt="">
+              <img :src="`/img/business-profile/application_form_2_${colorMode}_${locale}.png`" alt="">
             </div>
           </div>
         </div>
