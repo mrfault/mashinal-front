7602fb0c055d15fad7a73b6814933d4fec737176
Azer Mammadov <energon7777@gmail.com>
2022-02-02 12:01:19 +0400

    updates

---
+++ pages/notifications.vue
@@ -0,0 +1,24 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+
+  nuxtI18n: {
+    paths: {
+      az: '/bildirishler'
+    }
+  },
+  head() {
+    return this.$headMeta({
+      title: this.$t('notifications'),
+      description: this.$t('meta_desc_notifications')
+    });
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
