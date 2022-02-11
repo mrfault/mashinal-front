a28ea34d95ea29be254f6179e22d5b821a946451
Azer Mammadov <energon7777@gmail.com>
2022-02-11 12:15:43 +0400

    updates

--- components/elements/ActionBar.vue
+++ components/elements/ActionBar.vue
@@ -2,7 +2,7 @@
   <div class="action-bar d-lg-none">
     <div class="container">
       <div class="d-flex align-items-center justify-content-between">
-        <icon name="chevron-left" @click.native.stop="$emit('back')" class="cursor-pointer" />
+        <icon name="chevron-left" @click.native.stop="$router.back()" class="cursor-pointer" />
         <span>{{ title || $t('return_back') }}</span>
         <icon :name="actionIcon" @click.native.stop="$emit('action')" :class="['cursor-pointer', {'disabled':disableAction}]" v-if="actionIcon" />
         <icon name="check" @click.native.stop="$emit('accept')" class="cursor-pointer" v-else-if="showCheck" />
