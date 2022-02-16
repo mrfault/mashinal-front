8d563bd1d0925f33382db9b29aa824506063ae39
Mammad <mammad.g@al.ventures>
2022-02-16 18:26:18 +0400

    tolltip color change 01

--- components/options/ColorOptions.vue
+++ components/options/ColorOptions.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="color-options row">
-    <div class="col-2 col-lg-auto mb-2 mb-lg-3" v-for="(color, index) in colors" :key="color.id">
+    <div class="col-2 col-lg-auto mb-2 mb-lg-3 custom-tooltip" v-for="(color, index) in colors" :key="color.id">
       <span :class="['color', {'selected': isSelected(color.id), 'contrast': index > 8}]" :style="getStyles(color)" @click="selected = color.id" v-tooltip="color.name[locale]">
         <span class="sr-only">{{ color.name[locale] }}</span>
         <icon name="check" v-if="isSelected(color.id)" />
       </span>
     </div>
-    <div class="col-2 col-lg-auto mb-2 mb-lg-3" key="matt" v-if="!hideMatt">
+    <div class="col-2 col-lg-auto mb-2 mb-lg-3 custom-tooltip" key="matt" v-if="!hideMatt">
       <span :class="['color','matt', {'selected': matt}]" @click="$emit('change-matt', !matt)" v-tooltip="$t('matt')">
         <span>{{ $t('matt').slice(0,3) }}</span>
       </span>
     </div>
   </div>
 </template>
-
+<style lang="scss" >
+  .custom-tooltip {
+    .tooltip-inner {
+        background: #246EB2 !important;
+        color: #fff !important;
+      }
+  }
+ 
+</style>
 <script>
 import { mapGetters } from 'vuex';
 
