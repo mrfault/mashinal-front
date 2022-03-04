347124ef6e379c98f145258ade904c4f5a93d3fa
mammadgurbanov <mammad.g@al.ventures>
2022-03-04 17:18:42 +0400

    --fix

--- components/moto/MotoSearchForm.vue
+++ components/moto/MotoSearchForm.vue
@@ -25,7 +25,7 @@
             <div class="col-auto" v-if="!isMobileBreakpoint">
               <div class="form-counter">
                 <div class="form-info" v-if="canAddRow(index)" @click="addSearchRow(key)">
-                  <icon name="plus" />/
+                  <icon name="plus" />
                   <!-- <inline-svg src="/icons/plus.svg" :height="14" /> -->
                 </div>
                 <div class="form-info" v-if="canRemoveRow()" @click="removeSearchRow(key)">
