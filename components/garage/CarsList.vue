8b30e376d46ea0468c5dbdc82f91959dc10e24de
mammadgurbanov <mammad.g@al.ventures>
2022-03-17 16:09:49 +0400

    garage style layout --fix

--- components/garage/CarsList.vue
+++ components/garage/CarsList.vue
@@ -2,20 +2,33 @@
   <div class="row">
     <transition-expand>
       <div class="col-12" v-if="!cars.data.length && showNoActiveCarsAlert">
-        <div class="garage_no-active-cars d-flex align-items-center mb-2 mb-lg-3">
+        <div
+          class="garage_no-active-cars d-flex align-items-center mb-2 mb-lg-3"
+        >
           <img src="/img/speaker.svg" alt="" />
           <span class="text">{{ $t('no_active_cars') }}</span>
-          <span class="cursor-pointer ml-auto" @click="showNoActiveCarsAlert = false">
+          <span
+            class="cursor-pointer ml-auto"
+            @click="showNoActiveCarsAlert = false"
+          >
             <icon name="cross" />
             <!-- <inline-svg src="/icons/cross.svg" height="14"/> -->
           </span>
         </div>
       </div>
     </transition-expand>
-    <div class="col-12" v-show="carChosen && isMobileBreakpoint" v-if="activeCar">
+    <div
+      class="col-12"
+      v-show="carChosen && isMobileBreakpoint"
+      v-if="activeCar"
+    >
       <div class="garage_go-back card with-margins mb-2 mb-lg-0">
         <div class="d-flex align-items-center justify-content-between">
-          <icon name="chevron-left" @click.native.stop="showCarsList" class="cursor-pointer" />
+          <icon
+            name="chevron-left"
+            @click.native.stop="showCarsList"
+            class="cursor-pointer"
+          />
           <span>{{ $readCarNumber(activeCar.car_number) }}</span>
         </div>
       </div>
@@ -23,8 +36,16 @@
     <div class="col-12 col-lg-1-5" v-show="!carChosen || !isMobileBreakpoint">
       <div class="garage_cars-list mb-2 mb-lg-0">
         <div class="row">
-          <div class="col-6 col-lg-12" v-for="car in cars.data" :key="car.id">
-            <car-item :car="car" @set-active="updateActiveCar" :active="activeCarId === car.id" />
+          <div
+            class="col-6 col-lg-12 lg-xl-6"
+            v-for="car in cars.data"
+            :key="car.id"
+          >
+            <car-item
+              :car="car"
+              @set-active="updateActiveCar"
+              :active="activeCarId === car.id"
+            />
           </div>
           <div class="col-6 col-lg-12" v-if="isMobileBreakpoint">
             <add-car tag="div" />
@@ -33,71 +54,101 @@
       </div>
     </div>
     <div class="col-12 col-lg-4-5">
-      <div class="card with-margins" v-if="activeCar" v-show="carChosen || !isMobileBreakpoint">
-        <p class="p-title">{{$t('about')}}</p>
-        <car-info :car="activeCar"  :key="'info_' + activeCar.id" />
+      <div
+        class="card with-margins"
+        v-if="activeCar"
+        v-show="carChosen || !isMobileBreakpoint"
+      >
+        <p class="p-title">{{ $t('about') }}</p>
+        <car-info :car="activeCar" :key="'info_' + activeCar.id" />
       </div>
 
-      <div class="card with-margins mt-4" v-if="activeCar" v-show="carChosen || !isMobileBreakpoint">
-        <p class="p-title">{{$t('fines')}} </p>
-        <car-protocols :car="activeCar"  :key="'fines_' + activeCar.id" v-if="activeCar.car_id"/>
+      <div
+        class="card with-margins mt-4"
+        v-if="activeCar"
+        v-show="carChosen || !isMobileBreakpoint"
+      >
+        <p class="p-title">{{ $t('fines') }}</p>
+        <car-protocols
+          :car="activeCar"
+          :key="'fines_' + activeCar.id"
+          v-if="activeCar.car_id"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from 'vuex'
 
-import AddCar from '~/components/garage/AddCar';
-import CarItem from '~/components/garage/CarItem';
-import CarsNav from '~/components/garage/CarsNav';
-import CarInfo from '~/components/garage/CarInfo';
-import CarProtocols from '~/components/garage/CarProtocols';
+import AddCar from '~/components/garage/AddCar'
+import CarItem from '~/components/garage/CarItem'
+import CarsNav from '~/components/garage/CarsNav'
+import CarInfo from '~/components/garage/CarInfo'
+import CarProtocols from '~/components/garage/CarProtocols'
 
 export default {
   components: {
     AddCar,
     CarItem,
     CarInfo,
-    CarProtocols
+    CarProtocols,
   },
   data() {
-    let activeCars = this.$store.state.garage.cars.data?.filter(car => car.status === 1 && car.sync_status === 1);
+    let activeCars = this.$store.state.garage.cars.data?.filter(
+      (car) => car.status === 1 && car.sync_status === 1,
+    )
     return {
       tab: 'info',
       activeCarId: activeCars[0]?.id || '',
       showNoActiveCarsAlert: true,
-      carChosen: false
+      carChosen: false,
     }
   },
   computed: {
     ...mapGetters({
-      cars: 'garage/cars'
+      cars: 'garage/cars',
     }),
 
     activeCars() {
-      return this.cars.data?.filter(car => car.status === 1 && car.status === 1) || [];
+      return (
+        this.cars.data?.filter((car) => car.status === 1 && car.status === 1) ||
+        []
+      )
     },
     activeCar() {
-      return this.activeCars.find(car => car.id === this.activeCarId) || this.activeCars?.[0];
-    }
+      return (
+        this.activeCars.find((car) => car.id === this.activeCarId) ||
+        this.activeCars?.[0]
+      )
+    },
   },
   methods: {
     updateActiveCar(id) {
-      this.activeCarId = id;
-      this.carChosen = true;
-      this.$emit('show-nav', false);
+      this.activeCarId = id
+      this.carChosen = true
+      this.$emit('show-nav', false)
     },
     showCarsList() {
-      this.carChosen = false;
-      this.$emit('show-nav', true);
-    }
+      this.carChosen = false
+      this.$emit('show-nav', true)
+    },
   },
   watch: {
     'activeCars.length'() {
-      this.activeCarId = this.activeCar?.id || '';
-    }
-  }
+      this.activeCarId = this.activeCar?.id || ''
+    },
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+@media (min-width: 1025px) and (max-width: 1279px) {
+  .lg-xl-6 {
+    width: 50% !important;
+    padding: 0 15px !important;
+    max-width: 50%;
+  }
+}
+</style>
