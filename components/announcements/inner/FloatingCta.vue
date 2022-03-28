5a7afa3bf198cacb3e845a39e1287cd3c38cc791
mammadgurbanov <mammad.g@al.ventures>
2022-03-28 18:15:33 +0400

    login in monetization --feature --fix

--- components/announcements/inner/FloatingCta.vue
+++ components/announcements/inner/FloatingCta.vue
@@ -1,11 +1,24 @@
 <template>
-  <div v-if="!userIsOwner(announcement) || showMonetization(announcement) || announcement.has_monetization">
+  <div
+    v-if="
+      !userIsOwner(announcement) ||
+      showMonetization(announcement) ||
+      announcement.has_monetization
+    "
+  >
     <div class="floating-cta fixed-to-bottom" ref="floating">
       <div class="row">
         <div class="col" v-if="showMonetization(announcement)">
-          <monetization-button :announcement="announcement" class-name="red-outline" />
+          <monetization-button
+            @openModal="openModal"
+            :announcement="announcement"
+            class-name="red-outline"
+          />
         </div>
-        <div class="col" v-if="userIsOwner(announcement) && announcement.has_monetization">
+        <div
+          class="col"
+          v-if="userIsOwner(announcement) && announcement.has_monetization"
+        >
           <monetization-stats-button :announcement="announcement" />
         </div>
         <div class="col" v-if="!userIsOwner(announcement)">
@@ -15,34 +28,49 @@
     </div>
     <!-- <div ref="floatingBefore"></div>
     <div ref="floatingAfter"></div> -->
+    <modal-popup :toggle="showModal" @close="showModal = false">
+      <login-tabs></login-tabs>
+    </modal-popup>
   </div>
 </template>
 
 <script>
-import CallButton from '~/components/announcements/CallButton';
-import MonetizationButton from '~/components/announcements/MonetizationButton';
-import MonetizationStatsButton from '~/components/announcements/MonetizationStatsButton';
+import CallButton from '~/components/announcements/CallButton'
+import MonetizationButton from '~/components/announcements/MonetizationButton'
+import MonetizationStatsButton from '~/components/announcements/MonetizationStatsButton'
 
 export default {
   props: {
-    announcement: {}
+    announcement: {},
+  },
+  data() {
+    return {
+      showModal: false,
+    }
   },
   components: {
     CallButton,
     MonetizationButton,
-    MonetizationStatsButton
+    MonetizationStatsButton,
   },
   computed: {
     contact() {
-      return this.getAnnouncementContact(this.announcement);
+      return this.getAnnouncementContact(this.announcement)
     },
   },
   methods: {
     handleScroll() {
-      let bounds = this.$refs.floatingAfter.getBoundingClientRect();
-      this.$refs.floating.classList[bounds.top < 108 ? 'add' : 'remove']('fixed-to-top');
-      this.$refs.floating.classList[(bounds.top - window.innerHeight) > -82 ? 'add' : 'remove']('fixed-to-bottom');
-    }
+      let bounds = this.$refs.floatingAfter.getBoundingClientRect()
+      this.$refs.floating.classList[bounds.top < 108 ? 'add' : 'remove'](
+        'fixed-to-top',
+      )
+      this.$refs.floating.classList[
+        bounds.top - window.innerHeight > -82 ? 'add' : 'remove'
+      ]('fixed-to-bottom')
+    },
+    openModal() {
+      this.showModal = true
+    },
   },
   mounted() {
     // this.handleScroll();
@@ -52,6 +80,6 @@ export default {
   beforeDestroy() {
     // window.removeEventListener('scroll', this.handleScroll);
     // window.removeEventListener('resize', this.handleScroll);
-  }
+  },
 }
 </script>
