322f886f145298d1d2b982612336b945c5af8e59
mammadgurbanov <mammad.g@al.ventures>
2022-04-15 11:05:39 +0400

    navbar white hide

--- mixins/layout.js
+++ mixins/layout.js
@@ -1,8 +1,8 @@
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions } from 'vuex'
 
-import { ColorModeMixin } from '~/mixins/color-mode';
-import { SocketMixin } from '~/mixins/socket';
-import { MessagesMixin } from '~/mixins/messages';
+import { ColorModeMixin } from '~/mixins/color-mode'
+import { SocketMixin } from '~/mixins/socket'
+import { MessagesMixin } from '~/mixins/messages'
 
 export const LayoutMixin = {
   mixins: [ColorModeMixin, SocketMixin, MessagesMixin],
@@ -11,92 +11,121 @@ export const LayoutMixin = {
       vhVariableSet: false,
       showLoginPopup: false,
       loginActionKey: '',
-      loginInitialForm: {}
+      loginInitialForm: {},
     }
   },
   computed: {
-    ...mapGetters(['loading','messages','paidStatusData','hideFooter','notifications'])
+    ...mapGetters([
+      'loading',
+      'messages',
+      'paidStatusData',
+      'hideFooter',
+      'notifications',
+    ]),
   },
   methods: {
-    ...mapActions(['setLoading','setGridBreakpoint','getMessages','getNotifications','getFavorites','resetSellTokens','resetUserData','updatePaidStatus',
-                   'getNotViewedSavedSearch','getNotViewedFavorites']),
+    ...mapActions([
+      'setLoading',
+      'setGridBreakpoint',
+      'getMessages',
+      'getNotifications',
+      'getFavorites',
+      'resetSellTokens',
+      'resetUserData',
+      'updatePaidStatus',
+      'getNotViewedSavedSearch',
+      'getNotViewedFavorites',
+    ]),
 
     handleResize() {
       // update grid breakpoint
-      let breakpoint = false;
-      ['xs', 'sm', 'md', 'lg', 'xl'].map(name => {
-        let value = window.getComputedStyle(document.documentElement).getPropertyValue('--breakpoint-' + name);
+      let breakpoint = false
+      ;['xs', 'sm', 'md', 'lg', 'xl'].map((name) => {
+        let value = window
+          .getComputedStyle(document.documentElement)
+          .getPropertyValue('--breakpoint-' + name)
         if (window.matchMedia('(min-width: ' + value + ')').matches) {
-          breakpoint = name;
+          breakpoint = name
         }
-      });
+      })
       if (breakpoint !== this.breakpoint) {
-        this.setGridBreakpoint(breakpoint);
+        this.setGridBreakpoint(breakpoint)
       }
       // set calculated vh variable
-      if (this.vhVariableSet && this.isMobileDevice) return;
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-      this.vhVariableSet = true;
+      if (this.vhVariableSet && this.isMobileDevice) return
+      let vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+      this.vhVariableSet = true
     },
     handleScroll() {
-      console.log("scrolled");
-      let scrolled = window.scrollY;
-      let layout = document.querySelector('.layout');
+      console.log('scrolled')
+      let scrolled = window.scrollY
+      let layout = document.querySelector('.layout')
       // header
-      let headerEl = document.querySelector('.header-menu');
-      let menuHeaderEl = document.querySelector('.menu-header');
-      let promotionEl = document.querySelector('.top-promotion-row');
-      let headerElDesktopWhite = document.querySelector('.navbar-white');
-      promotionEl?.classList[window.scrollY > 0 ? 'add' : 'remove']('d-none');
-      headerElDesktopWhite?.classList[window.scrollY > 0 ? 'add' : 'remove']('d-none');
-      [headerEl, menuHeaderEl].map(el => {
-        el?.classList[window.scrollY > 0 ? 'add' : 'remove']('has-shadow');
-      });
+      let headerEl = document.querySelector('.header-menu')
+      let menuHeaderEl = document.querySelector('.menu-header')
+      let promotionEl = document.querySelector('.top-promotion-row')
+      let headerElDesktopWhite = document.querySelector('.navbar-white')
+      promotionEl?.classList[window.scrollY > 0 ? 'add' : 'remove']('d-none')
+      setTimeout(() => {
+        headerElDesktopWhite?.classList[window.scrollY > 0 ? 'add' : 'remove'](
+          'd-none',
+        )
+      }, 2000)
+      ;[headerEl, menuHeaderEl].map((el) => {
+        el?.classList[window.scrollY > 0 ? 'add' : 'remove']('has-shadow')
+      })
       // header white desktop
       // footer
-      let footerEl = document.querySelector('.page-footer');
+      let footerEl = document.querySelector('.page-footer')
       if (footerEl) {
         this.$nextTick(() => {
-          let reachedFooter = (window.pageYOffset + window.innerHeight) >= footerEl.offsetTop;
-          let noScroll = document.documentElement.clientHeight === window.innerHeight;
-          layout.classList[(scrolled > 0 || noScroll) && reachedFooter ? 'add' : 'remove']('reached-footer');
-          layout.classList[scrolled > 0 ? 'add' : 'remove']('scrolled');
-        });
+          let reachedFooter =
+            window.pageYOffset + window.innerHeight >= footerEl.offsetTop
+          let noScroll =
+            document.documentElement.clientHeight === window.innerHeight
+          layout.classList[
+            (scrolled > 0 || noScroll) && reachedFooter ? 'add' : 'remove'
+          ]('reached-footer')
+          layout.classList[scrolled > 0 ? 'add' : 'remove']('scrolled')
+        })
       }
     },
     // login
     toggleEchoListening(toggle) {
       if (toggle) {
-        this.connectEcho().listen('SendMessage', this.addNewMessage);
-        this.connectEcho('global-channel.'+this.$auth.user.id).listen('GlobalFrontEndEvent',({ data }) => {
-          data.map(value => {
-            this.$store.dispatch(value)
-          })
-        })
+        this.connectEcho().listen('SendMessage', this.addNewMessage)
+        this.connectEcho('global-channel.' + this.$auth.user.id).listen(
+          'GlobalFrontEndEvent',
+          ({ data }) => {
+            data.map((value) => {
+              this.$store.dispatch(value)
+            })
+          },
+        )
       } else if (window.Echo) {
-        this.connectEcho().stopListening('SendMessage');
+        this.connectEcho().stopListening('SendMessage')
       }
     },
     async getUserData() {
-      if (!this.loggedIn) return;
-      if (!this.messages.length) await this.getMessages();
-      if(!this.notifications.length) await this.getNotifications();
+      if (!this.loggedIn) return
+      if (!this.messages.length) await this.getMessages()
+      if (!this.notifications.length) await this.getNotifications()
       await Promise.all([
         this.getNotViewedSavedSearch(),
         this.getNotViewedFavorites(),
-        this.getFavorites()
-      ]);
+        this.getFavorites(),
+      ])
     },
     closeLogin() {
-      this.showLoginPopup = false;
-      this.loginActionKey = '';
+      this.showLoginPopup = false
+      this.loginActionKey = ''
     },
     handleAfterLogin() {
-      let key = this.loginActionKey;
-      this.closeLogin();
-      this.resetSellTokens();
-      if (key) this.$nuxt.$emit('after-login', key);
+      let key = this.loginActionKey
+      this.closeLogin()
+      this.resetSellTokens()
+      if (key) this.$nuxt.$emit('after-login', key)
     },
     // registerSW() {
     //   if ('serviceWorker' in navigator) {
@@ -108,64 +137,62 @@ export const LayoutMixin = {
     //this.getUserData();
 
     this.$nuxt.$on('login', (auth) => {
-      this.getUserData();
+      this.getUserData()
       if (!auth) {
         // reset store auth data
-        this.resetUserData();
+        this.resetUserData()
       }
-    });
+    })
   },
   mounted() {
     this.$nextTick(() => {
-      this.configSocket();
-      if (this.loggedIn)
-        this.toggleEchoListening(true);
+      this.configSocket()
+      if (this.loggedIn) this.toggleEchoListening(true)
 
       this.$nuxt.$on('login', (auth) => {
-        if (auth) this.handleAfterLogin();
-        this.toggleEchoListening(auth);
-      });
+        if (auth) this.handleAfterLogin()
+        this.toggleEchoListening(auth)
+      })
 
       this.$nuxt.$on('login-popup', (key, form = {}) => {
-        if (this.loggedIn) return;
-        this.showLoginPopup = true;
-        this.loginActionKey = key;
-        this.$set(this, 'loginInitialForm', form);
-      });
+        if (this.loggedIn) return
+        this.showLoginPopup = true
+        this.loginActionKey = key
+        this.$set(this, 'loginInitialForm', form)
+      })
 
-      window.addEventListener('resize', this.handleResize);
-      window.addEventListener('resize', this.handleScroll);
-      window.addEventListener('scroll', this.handleScroll);
+      window.addEventListener('resize', this.handleResize)
+      window.addEventListener('resize', this.handleScroll)
+      window.addEventListener('scroll', this.handleScroll)
       // window.addEventListener('load', this.registerSW);
 
       setTimeout(() => {
-        this.handleResize();
-        this.handleScroll();
-        this.pickColorMode();
+        this.handleResize()
+        this.handleScroll()
+        this.pickColorMode()
         // check payment status
-        if (['true','false'].includes(this.$route.query.success)) {
-          let type = this.$route.query.success === 'true' ? 'success' : 'error';
+        if (['true', 'false'].includes(this.$route.query.success)) {
+          let type = this.$route.query.success === 'true' ? 'success' : 'error'
           this.updatePaidStatus({
             type,
             title: this.$t(`${type}_payment`),
             text: '', //this.$t(`${type}_payment_msg`)
-          });
+          })
         }
         // strange behavior of loading prop which is not updating
         // in v-show directive without changing key sometimes
-        this.setLoading(false);
-      }, 0);
-    });
+        this.setLoading(false)
+      }, 0)
+    })
   },
   beforeDestroy() {
-    if (this.loggedIn)
-      this.toggleEchoListening(false);
+    if (this.loggedIn) this.toggleEchoListening(false)
 
-    this.$nuxt.$off(['login', 'login-popup']);
+    this.$nuxt.$off(['login', 'login-popup'])
 
-    window.removeEventListener('resize', this.handleResize);
-    window.removeEventListener('resize', this.handleScroll);
-    window.removeEventListener('scroll', this.handleScroll);
+    window.removeEventListener('resize', this.handleResize)
+    window.removeEventListener('resize', this.handleScroll)
+    window.removeEventListener('scroll', this.handleScroll)
     // window.removeEventListener('load', this.registerSW);
-  }
+  },
 }
