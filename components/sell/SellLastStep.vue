a91eb4411fee874f2c801ef8f6b91b1bd3b0637b
Azer Mammadov <energon7777@gmail.com>
2022-09-09 17:19:12 +0400

    updates

--- components/sell/SellLastStep.vue
+++ components/sell/SellLastStep.vue
@@ -136,7 +136,7 @@
                     @change="removeError('end_date')"
                     date-type="datetime"
                     value-type="datetime"
-                    date-format="DD.MM.YYYY HH"
+                    date-format="DD.MM.YYYY HH:00"
                     v-model="form.end_date"
                     :placeholder="$t('announcement_end_date')"
                     input-date
