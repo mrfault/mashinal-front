b476c9c0e097a27dc01f11a593208b7b9520e36e
Azer Mammadov <energon7777@gmail.com>
2022-09-15 10:04:14 +0400

    updates

--- pages/external-salons/index.vue
+++ pages/external-salons/index.vue
@@ -13,7 +13,7 @@
             v-model="searchFormType"
           />
         </div>
-        <salon-search-form v-show="searchFormType === 1" />
+        <salon-search-form :show-barter="false" v-show="searchFormType === 1" />
         <salon-filters-form
           v-show="searchFormType === 0"
           @filter="showSearch = false"
@@ -26,7 +26,7 @@
       <div class="container">
         <breadcrumbs :crumbs="crumbs" />
         <template v-if="!isMobileBreakpoint">
-          <salon-search-form />
+          <salon-search-form :show-barter="false" />
           <salon-filters-form :count="salonsFiltered.length" />
         </template>
         <banners type="2" class="mt-5" />
