836a77c3c5923dd9b8d2c630751926c7073b9b45
mammadgurbanov <mammad.g@al.ventures>
2022-10-06 12:26:20 +0400

    moderator init

---
+++ pages/moderator/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>Moderator</h1>
+    <sell-select-modification />
+  </div>
+</template>
+
+<script>
+import SellSelectModification from '@/components/sell/SellSelectModification'
+export default {
+    components:{
+        SellSelectModification
+    }
+}
+</script>
+
+<style></style>
