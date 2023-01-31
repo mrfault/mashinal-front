34a8d35fb9066245fa9194a56bea9d4cbe1a7292
mammadgurbanov <mammad.g@al.ventures>
2023-01-31 17:24:12 +0400

    moderation config options --fix 5

--- components/moderator/RadioGroup.vue
+++ components/moderator/RadioGroup.vue
@@ -1,7 +1,11 @@
 <template>
-<div class="row">
-  <form-radio class="mt-2 col-12 col-lg-4"  :radio-value="option.key" v-model="computedValue" :label="$t(option.name)" v-for="(option,index) in options" :id="option.key + 'ma-moderation' + option.name + keyItem" bigLabel/>
-</div>
+  <div class="row">
+    <form-radio v-for="(option,index) in options"
+                :id="`${option.key}-${option.name}-${keyItem}-${keyValue}`"
+                :key="keyValue+keyValue" v-model="computedValue"
+                :label="$t(option.name)" :radio-value="option.key"
+                bigLabel class="mt-2 col-12 col-lg-4"/>
+  </div>
 </template>
 
 <script>
@@ -9,7 +13,7 @@ import TitleWithLineAndRejectReason from '~/components/moderator/titleWithLineAn
 
 export default {
   name: "RadioGroup",
-  components:{
+  components: {
     TitleWithLineAndRejectReason
   },
   props: {
