f699a444e73ba6426d8cdf56634519b61148074e
Azer Mammadov <energon7777@gmail.com>
2023-02-21 12:51:05 +0400

    updates

---
+++ components/announcements/CallButtonMultiple.vue
@@ -0,0 +1,76 @@
+<template>
+  <button v-if="!showPhone" :class="['btn full-width', `btn--${callAtOnce ? '' : 'pale-'}green`]" @click.stop="handleClick">
+    <icon name="phone-call" />
+    <template v-if="callAtOnce">
+      <template v-for="phone in phones" v-if="!isMobileBreakpoint">
+        <span  v-mask="$maskPhone(true)" >+{{ phone }}</span>
+        <br>
+      </template>
+
+      <span v-else>{{ $t('make_a_call') }}</span>
+    </template>
+    <template v-else>
+      +994 *******
+    </template>
+  </button>
+  <div v-else>
+    <template v-if="callAtOnce">
+      <template v-for="phone in phones" v-if="!isMobileBreakpoint">
+        <a class="call-a" style="" :href="`tel:+${phone}`" v-mask="$maskPhone(true)" >+{{ phone }}</a>
+        <br>
+      </template>
+
+      <span v-else>{{ $t('make_a_call') }}</span>
+    </template>
+  </div>
+</template>
+<style lang="scss">
+.call-a {
+  font-size: 16px;
+  color: #081A3E;
+  font-weight: 500;
+  &:hover {
+    color: #2aa53e;
+  }
+}
+</style>
+<script>
+export default {
+  props: {
+    phones: {},
+    announcementId: {
+      type: [String, Number],
+      required: false
+    }
+  },
+  data() {
+    return {
+      showPhone: false
+    }
+  },
+  computed: {
+    callAtOnce() {
+      return this.showPhone || this.isMobileBreakpoint;
+    },
+    id() {
+      return this.$route.params.id || this.announcementId
+    }
+  },
+  methods: {
+    handleClick() {
+      if (this.callAtOnce) {
+        window.location.href = `tel:+${this.phone}`;
+        this.trackCall(2);
+      } else {
+        this.showPhone = true;
+        this.trackCall(1);
+      }
+    },
+    trackCall(n) {
+      this.fbTrack('Call ' + n);
+      this.gtagTrack('AW-600951956/' + (n === 1 ? 'rgWNCOTA8IMCEJSZx54C' : 'VunMCPr51oMCEJSZx54C'));
+      this.$axios.$get(`/announce/${this.id}/show/phone`);
+    }
+  }
+}
+</script>
