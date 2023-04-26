5b81f54b25b52d531f9272defaa834b249c53a47
maratoid <maratoid@mail.ru>
2023-04-26 15:10:20 +0400

    Packages

---
+++ pages/profile/packages.vue
@@ -0,0 +1,27 @@
+<template>
+   <div class="myPackages">
+      <div class="container">
+         <breadcrumbs :crumbs="crumbs"/>
+
+      </div>
+   </div>
+</template>
+
+<script>
+   export default {
+      computed: {
+         crumbs() {
+            return [
+               { name: this.$t('dashboard'), route: '/dashboard/3' },
+               { name: this.$t('my_packages') }
+            ]
+         }
+      }
+   }
+</script>
+
+<style lang="scss" scoped>
+   .myPackages {
+
+   }
+</style>
