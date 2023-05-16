cdee99ff06c3e6dd6bea5db6c4e8060a0ef87e51
maratoid <maratoid@mail.ru>
2023-05-16 09:48:35 +0400

    Initial settings

---
+++ components/elements/IconNovelties.vue
@@ -0,0 +1,22 @@
+<template>
+    <span class="iconNovelties">
+        {{ locale === 'az' ? 'yeni' : 'новинка' }}
+    </span>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.iconNovelties {
+   height: 18px;
+   padding: 0 6px;
+   font-weight: 500;
+   font-size: 13px;
+   line-height: 16px;
+   color: #FFFFFF;
+   border-radius: 2px;
+   background-color: #F81734;
+}
+</style>
