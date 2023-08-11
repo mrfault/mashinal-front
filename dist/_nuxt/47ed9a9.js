ff574eb9e40fd386b23b6c72a1f376247f727534
beycanbeycanov <bbeycanov@gmail.com>
2023-08-11 14:05:23 +0400

    changes

--- dist/_nuxt/47ed9a9.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[53],{1522:function(n,e,t){"use strict";t.r(e);var l={name:"page-dashboard-index",middleware:"auth_salon",nuxtI18n:{paths:{az:"/idareetme-paneli"}}},d=t(12),component=Object(d.a)(l,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);e.default=component.exports}}]);
