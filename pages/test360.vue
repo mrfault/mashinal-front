d24f199237c1be0f9748fe2b92daa66d4cf0868a
maratoid <maratoid@mail.ru>
2023-08-22 15:59:24 +0400

    Fix bugs

--- pages/test360.vue
+++ pages/test360.vue
@@ -11,52 +11,53 @@
 </template>
 
 <script>
-export default {
-   head() {
-      return {
-         script: [
-            {
-               src: 'https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.js',
-               body: true,
-               async: true,
-               defer: true
-            }
-         ],
-         link: [{ rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.css' }]
-      }
-   },
-   mounted() {
-      let interval = setInterval(() => {
-         if (window.pannellum) {
-            pannellum.viewer('panorama', {
-               "type": "equirectangular",
-               "panorama": "http://10.20.120.187:3000/360/test2.png",
-               "autoLoad": true,
-               "autoRotate": -8,
-               "compass": false,
-               "autoRotateInactivityDelay": 5,
-               "mouseZoom": false,
-               "keyboardZoom": false,
-               "showZoomCtrl": false,
-               "showControls": false,
-               "draggable": false,
-               "yaw": 0,
-               "pitch": 0,
-               "hfov": 100,
-               "avoidShowingBackground": false,
-               "backgroundColor": [0,0,0]
-            });
-
-            clearInterval(interval);
+   export default {
+      head() {
+         return {
+            script: [
+               {
+                  src: 'https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.js',
+                  body: true,
+                  async: true,
+                  defer: true
+               }
+            ],
+            link: [{ rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.css' }]
          }
-      }, 1000);
+      },
+
+      mounted() {
+         let interval = setInterval(() => {
+            if (window.pannellum) {
+               pannellum.viewer('panorama', {
+                  "type": "equirectangular",
+                  "panorama": "http://10.20.120.139:2000/360/test2.png",
+                  "autoLoad": true,
+                  "autoRotate": -8,
+                  "compass": true,
+                  // "autoRotateInactivityDelay": 5,
+                  // "mouseZoom": false,
+                  // "keyboardZoom": false,
+                  // "showZoomCtrl": false,
+                  // "showControls": false,
+                  // "draggable": false,
+                  // "yaw": 0,
+                  // "pitch": 0,
+                  // "hfov": 100,
+                  // "avoidShowingBackground": false,
+                  // "backgroundColor": [0,0,0]
+               });
+
+               clearInterval(interval);
+            }
+         }, 1000);
+      }
    }
-}
 </script>
 
 <style scoped>
-#panorama {
-   width: 100%;
-   height: 500px;
-}
+   #panorama {
+      width: 100%;
+      height: 500px;
+   }
 </style>
