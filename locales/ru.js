b5ab7c5f2049f1ec0755ef9b541d5dd9f4d464ae
maratoid <maratoid@mail.ru>
2023-08-28 04:50:44 +0400

    Fix bugs

--- locales/ru.js
+++ locales/ru.js
@@ -2120,6 +2120,7 @@ export default {
    with_mileage: "С пробегом",
    with_mileage_2: "С пробегом",
    with_new: "новых",
+   used: "Б/У",
    with_significant_damage_to_body_elements_that_do_not_move_on_their_own:
       "С существенными повреждениями кузовных элементов, не передвигающиеся своим ходом.",
    with_video: "360&deg;",
